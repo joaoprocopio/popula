843fab0e83e21e3c137e1c40f1629f5371b45af7
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-05-24 19:05:18 -0300

    remove mobile

--- playwright.config.js
+++ playwright.config.js
@@ -27,18 +27,9 @@ export default defineConfig({
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
-
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
-    },
-    {
-      name: 'Mobile Chrome',
-      use: { ...devices['Pixel 5'] },
-    },
-    {
-      name: 'Mobile Safari',
-      use: { ...devices['iPhone 12'] },
     },
   ],
   webServer: {
