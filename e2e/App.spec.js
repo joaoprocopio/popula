9734a8bda9d707ed16bca88baa91bec133f4bfe0
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-05-25 14:47:36 -0300

    refactor e2e test

--- e2e/App.spec.js
+++ e2e/App.spec.js
@@ -1,23 +1,32 @@
 import { test, expect } from '@playwright/test'
 
-// const city = page.getByText('São José dos Campos')
-// const input = page.locator('.v-field__input input')
-// const geometries = page.locator('.leaflet-interactive')
 // const chart = page.locator('.v-card-text canvas')
 
 test.describe('App', () => {
+  let searchbar
+  let cities
+  let geometries
+  let chart
+
   test.beforeEach(async ({ page }) => {
+    searchbar = page.locator('.v-field__input input')
+    cities = page.locator(
+      '.v-autocomplete__content .v-list .v-list-item .v-list-item__content .v-list-item-title'
+    )
+    geometries = page.locator('.leaflet-interactive')
+    chart = page.locator('.v-card-text canvas')
+
     await page.goto('/')
   })
 
-  test('Search for a city and a city option is rendered', async ({ page }) => {
-    const input = page.locator('.v-field__input input')
-    const city = page.getByText('São José dos Campos')
+  test('Search for a city', async () => {
+    const city = cities.first()
 
-    await input.fill('sao jose')
+    await searchbar.fill('sao jose dos campos')
 
     await city.waitFor({ state: 'visible' })
 
+    expect(await city.textContent()).toContain('São José dos Campos')
     expect(await city.isVisible()).toBeTruthy()
   })
 })
